02d49eaf8dc0e9202555f71f6ea15efd679053fd
Juanca_0011 <juan.londono491@pascualbravo.edu.co>
2025-02-18 10:48:36 -0500

    cree persistecnia

---
+++ CampeonatosFIFA.Persistencia/Contexto/CampeonatosFIFAContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CampeonatosFIFA.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace CampeonatosFIFA.Persistencia.Contexto
+{
+    public class CampeonatosFIFAContext : DbContext
+    {
+        public CampeonatosFIFAContext(DbContextOptions<CampeonatosFIFAContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet <Seleccion> Seleciones { get; set; }
+        public DbSet < Campeonato> Campeonatos { get; set; }
+
+        void onModelCreating(ModelBuilder builder) 
+        { 
+          builder.Entity<Seleccion>(entidad => { 
+          
+          entidad.HasKey(e => e.Id);
+          entidad.HasIndex(e => e.Nombre).IsUnique();
+          });
+
+          builder.Entity<Seleccion>(entidad =>
+            {
+              entidad.HasKey(e => e.Id);
+              entidad.HasIndex(e => e.Nombre).IsUnique();
+            });
+
+            // como se define la clave foranea: campeonato depende de seleccion y de pais
+            builder.Entity<Campeonato>()
+                .HasOne(e => e.PaisOrganizador)
+                .WithMany()
+                .HasForeignKey(e => e.IdSeleccion);
+
+
+        }
+
+         
+
+    }
+}
--- CampeonatosFIFA.Persistencia/obj/Debug/net8.0/CampeonatosFIFA.Persistencia.AssemblyInfo.cs
+++ CampeonatosFIFA.Persistencia/obj/Debug/net8.0/CampeonatosFIFA.Persistencia.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CampeonatosFIFA.Dominio")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CampeonatosFIFA.Persistencia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+202001f2ed05f2a3c575a38ede7a1faa1dda985c")]
-[assembly: System.Reflection.AssemblyProductAttribute("CampeonatosFIFA.Dominio")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CampeonatosFIFA.Dominio")]
+[assembly: System.Reflection.AssemblyProductAttribute("CampeonatosFIFA.Persistencia")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CampeonatosFIFA.Persistencia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
